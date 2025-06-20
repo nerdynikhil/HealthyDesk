fad4a31cdfafa3bb7496ec91c7bb0c117fa9a65f
Nikhil Barik <nklmarch17@gmail.com>
2025-06-20 09:30:43 +0530

    Initial Commit

---
+++ HealthyDesk/HealthyDeskApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthyDeskApp.swift
+//  HealthyDesk
+//
+//  Created by Nikhil Barik on 20/06/25.
+//
+
+import SwiftUI
+
+@main
+struct HealthyDeskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
